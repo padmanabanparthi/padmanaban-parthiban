d76ac734ca3d88cf5060b771ca8f86fa001871bd
LAPTOP-FFDC3P59\Nikitha Sri <parthistarpk89@gmail.com>
2018-07-16 22:04:42 +0530

    use another gallery filter for recent woks

--- src/app/myworks/myworks.component.ts
+++ src/app/myworks/myworks.component.ts
@@ -11,6 +11,30 @@ export class MyworksComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function() {
+
+      $(".filter-button").click(function() {
+          var value = $(this).attr('data-filter');
+          if(value == "all")
+          {
+              //$('.filter').removeClass('hidden');
+              $('.filter').show();
+          }
+          else {
+            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide();
+            $('.filter').filter('.'+value).show();
+            $('.filter').filter('.'+value).addClass("animated flipInX");
+          }
+      });
+      
+      if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+      }
+      $(this).addClass("active");
+    
+    });
   }
 
 }
